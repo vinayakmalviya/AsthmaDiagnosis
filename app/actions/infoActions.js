d865a3988bb623a21528130acacaae58f7871e06
janvi-vp <janvi.vp@somaiya.edu>
2019-12-22 19:48:17 +0530

    Added action

--- app/actions/infoActions.js
+++ app/actions/infoActions.js
@@ -3,6 +3,7 @@ export const HANDLE_PERSONAL_INFO = 'HANDLE_PERSONAL_INFO';
 export const HANDLE_FAMILY_INFO = 'HANDLE_FAMILY_INFO';
 export const HANDLE_SYMPTOMS = 'HANDLE_SYMPTOMS';
 export const HANDLE_INVESTIGATIONS = 'HANDLE_INVESTIGATIONS';
+export const HANDLE_COMORBIDITIES = 'HANDLE_COMORBIDITIES';
 
 export const testSubmit = values => ({
     type: HANDLE_TEST_ACTION,
@@ -27,5 +28,10 @@ export const symptomsSubmit = values => ({
 
 export const investigationsSubmit = values => ({
     type: HANDLE_INVESTIGATIONS,
+    values,
+});
+
+export const comorbiditiesSubmit = values => ({
+    type: HANDLE_COMORBIDITIES,
     values,
 })
