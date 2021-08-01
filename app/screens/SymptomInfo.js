724c0e48382f6998baf330b2a651c0379af77a8c
Vinayak Malviya <vinayak.malviya@somaiya.edu>
2021-08-01 12:11:38 +0530

    :bug: Critical bugfix

--- app/screens/SymptomInfo.js
+++ app/screens/SymptomInfo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View, Text } from "react-native";
 import { DataTable, Divider } from "react-native-paper";
 
@@ -8,17 +8,29 @@ import { connect } from "react-redux";
 import { Title, CustomSubTitle } from "../components/Text";
 import { CustomOverline } from "../components/Text";
 
+const severity = [
+	"Intermittent",
+	"Persistent: Mild",
+	"Persistent: Moderate",
+	"Persistent: Severe",
+];
+
+const name = [
+	"No Occurence",
+	"2 Days A Week",
+	"Daily",
+	"Multiple Times In A Day",
+];
+
+const spiro = ["Normal", "Between 60%\nto 80%", "Less than\n60%"];
+
+const treat = ["Continue Same", "Step Up", "Step Down"];
+
 const SymptonInfo = (props) => {
 	const patientName = props.vals.name || "Name";
 	const patientAge = props.vals.age || "Age";
 	const patientGender = props.vals.gender || "Gender";
-	const name = [
-		"No Occurence",
-		"2 Days A Week",
-		"Daily",
-		"Multiple Times In A Day",
-	];
-	const spiro = ["Normal", "Between 60%\nto 80%", "Less than\n60%"];
+
 	let dns = "";
 	let pharyn = "";
 	let rhonchi = "";
@@ -31,78 +43,75 @@ const SymptonInfo = (props) => {
 	let food = "";
 	let pollen = "";
 	let others = "";
-	if (props.vals.comorbidities.urt_findings) {
-		dns = props.vals.comorbidities.urt_findings.dns
+
+	if (props.vals?.comorbidities?.urt_findings) {
+		dns = props.vals.comorbidities?.urt_findings?.dns
 			? "Deviated Nasal Septum "
 			: "";
-		pharyn = props.vals.comorbidities.urt_findings.pharyn
+		pharyn = props.vals.comorbidities?.urt_findings?.pharyn
 			? "Pharyngitis "
 			: "";
-		rhonchi = props.vals.comorbidities.urt_findings.rhonchi
+		rhonchi = props.vals.comorbidities?.urt_findings?.rhonchi
 			? "Rhonchi "
 			: "";
-		pnd = props.vals.comorbidities.urt_findings.pnd
+		pnd = props.vals.comorbidities?.urt_findings?.pnd
 			? "Post Nasal Drip "
 			: "";
-		hpt = props.vals.comorbidities.urt_findings.hpt
+		hpt = props.vals.comorbidities?.urt_findings?.hpt
 			? "Hypertrophic Turbinates "
 			: "";
-		nps = props.vals.comorbidities.urt_findings.nps ? "Nasal Polyps " : "";
-		ear_dis = props.vals.comorbidities.urt_findings.ear_dis
+		nps = props.vals.comorbidities?.urt_findings?.nps
+			? "Nasal Polyps "
+			: "";
+		ear_dis = props.vals.comorbidities?.urt_findings?.ear_dis
 			? "Ear Discharge "
 			: "";
 	}
-	if (props.vals.tests.skin_prick) {
-		fungal = props.vals.tests.skin_prick.fungal
-			? props.vals.tests.skin_prick.fungal.selected
-				? props.vals.tests.skin_prick.fungal.value + " "
+
+	if (props.vals.tests?.skin_prick) {
+		fungal = props.vals.tests?.skin_prick?.fungal
+			? props.vals.tests?.skin_prick.fungal?.selected
+				? props.vals.tests?.skin_prick.fungal?.value + " "
 				: ""
 			: "";
-		insect = props.vals.tests.skin_prick.insect
-			? props.vals.tests.skin_prick.insect.selected
-				? props.vals.tests.skin_prick.insect.value + " "
+		insect = props.vals.tests?.skin_prick?.insect
+			? props.vals.tests?.skin_prick.insect?.selected
+				? props.vals.tests?.skin_prick.insect?.value + " "
 				: ""
 			: "";
-		food = props.vals.tests.skin_prick.food
-			? props.vals.tests.skin_prick.food.selected
-				? props.vals.tests.skin_prick.food.value + " "
+		food = props.vals.tests?.skin_prick?.food
+			? props.vals.tests?.skin_prick.food?.selected
+				? props.vals.tests?.skin_prick.food?.value + " "
 				: ""
 			: "";
-		pollen = props.vals.tests.skin_prick.pollen
-			? props.vals.tests.skin_prick.pollen.selected
-				? props.vals.tests.skin_prick.pollen.value + " "
+		pollen = props.vals.tests?.skin_prick?.pollen
+			? props.vals.tests?.skin_prick.pollen?.selected
+				? props.vals.tests?.skin_prick.pollen?.value + " "
 				: ""
 			: "";
-		others = props.vals.tests.skin_prick.others
-			? props.vals.tests.skin_prick.others.selected
-				? props.vals.tests.skin_prick.others.value + " "
+		others = props.vals.tests?.skin_prick?.others
+			? props.vals.tests?.skin_prick.others?.selected
+				? props.vals.tests?.skin_prick.others?.value + " "
 				: ""
 			: "";
 	} else {
 		others = "No data recorded";
 	}
-	const severity = [
-		"Intermittent",
-		"Persistent: Mild",
-		"Persistent: Moderate",
-		"Persistent: Severe",
-	];
 
-	const treat = ["Continue Same", "Step Up", "Step Down"];
-
-	const renderInhaler = () => {
+	const renderInhaler = useCallback(() => {
 		let inhaler = "";
-		if (props.vals.diagnosis.inhaler.ics) {
+		if (props.vals.diagnosis?.inhaler?.ics) {
 			inhaler = inhaler.concat(" ", "ICS");
 		}
-		if (props.vals.diagnosis.inhaler.laba) {
+		if (props.vals.diagnosis?.inhaler?.laba) {
 			inhaler = inhaler.concat(" ", "LABA");
 		}
-		if (props.vals.diagnosis.inhaler.lama) {
+		if (props.vals.diagnosis?.inhaler?.lama) {
 			inhaler = inhaler.concat(" ", "LAMA");
 		}
+
 		return <Text style={{ marginTop: 6 }}>{inhaler}</Text>;
-	};
+	}, [props.vals]);
 
 	return (
 		<ScreenTemplate>
@@ -117,7 +126,7 @@ const SymptonInfo = (props) => {
 							<CustomOverline text="First Checkup: " />
 							<Text style={{ marginTop: 6 }}>
 								{props.vals.ini_symptoms.date
-									? props.vals.ini_symptoms.date
+									? props.vals.ini_symptoms?.date
 									: "No data present"}
 							</Text>
 						</RowView>
@@ -126,11 +135,11 @@ const SymptonInfo = (props) => {
 								<RowView>
 									<CustomOverline text="Diagnosis: " />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.severity
+										{props.vals.diagnosis?.severity
 											? severity[
 													parseInt(
 														props.vals.diagnosis
-															.severity
+															?.severity
 													)
 											  ]
 											: "No data present"}
@@ -139,7 +148,7 @@ const SymptonInfo = (props) => {
 								<RowView>
 									<CustomOverline text="Inhaler: " />
 
-									{props.vals.diagnosis.inhaler ? (
+									{props.vals.diagnosis?.inhaler ? (
 										renderInhaler()
 									) : (
 										<Text style={{ marginTop: 6 }}>
@@ -150,48 +159,48 @@ const SymptonInfo = (props) => {
 								<RowView>
 									<CustomOverline text="Dosage:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.dosage
-											? props.vals.diagnosis.dosage
+										{props.vals.diagnosis?.dosage
+											? props.vals.diagnosis?.dosage
 											: "No data present"}
 									</Text>
 								</RowView>
 								<RowView>
 									<CustomOverline text="Immunotherapy:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.immuno
-											? props.vals.diagnosis.immuno
+										{props.vals.diagnosis?.immuno
+											? props.vals.diagnosis?.immuno
 											: "Not prescribed"}
 									</Text>
 								</RowView>
 								<RowView>
 									<CustomOverline text="Oral Medications:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.oral_m
-											? props.vals.diagnosis.oral_m
+										{props.vals.diagnosis?.oral_m
+											? props.vals.diagnosis?.oral_m
 											: "Not prescribed"}
 									</Text>
 								</RowView>
 								<RowView>
 									<CustomOverline text="Biological Medications:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.bio_m
-											? props.vals.diagnosis.bio_m
+										{props.vals.diagnosis?.bio_m
+											? props.vals.diagnosis?.bio_m
 											: "Not prescribed"}
 									</Text>
 								</RowView>
 								<RowView>
 									<CustomOverline text="Supportive Therapy:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.supp
-											? props.vals.diagnosis.supp
+										{props.vals.diagnosis?.supp
+											? props.vals.diagnosis?.supp
 											: "Not prescribed"}
 									</Text>
 								</RowView>
 								<RowView>
 									<CustomOverline text="Other Specifications:" />
 									<Text style={{ marginTop: 6 }}>
-										{props.vals.diagnosis.specs
-											? props.vals.diagnosis.specs
+										{props.vals.diagnosis?.specs
+											? props.vals.diagnosis?.specs
 											: "None"}
 									</Text>
 								</RowView>
@@ -206,8 +215,8 @@ const SymptonInfo = (props) => {
 					<RowView>
 						<CustomOverline text="First Checkup: " />
 						<Text style={{ marginTop: 6 }}>
-							{props.vals.ini_symptoms.date
-								? props.vals.ini_symptoms.date
+							{props.vals.ini_symptoms?.date
+								? props.vals.ini_symptoms?.date
 								: "No data recorded"}
 						</Text>
 					</RowView>
@@ -219,7 +228,7 @@ const SymptonInfo = (props) => {
 						<DataTable.Row>
 							<DataTable.Cell>Wheezing</DataTable.Cell>
 							<DataTable.Cell>
-								{name[props.vals.ini_symptoms.wheezing]}
+								{name[props.vals.ini_symptoms?.wheezing]}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
@@ -228,7 +237,7 @@ const SymptonInfo = (props) => {
 								{
 									name[
 										props.vals.ini_symptoms
-											.shortness_of_breath
+											?.shortness_of_breath
 									]
 								}
 							</DataTable.Cell>
@@ -236,13 +245,13 @@ const SymptonInfo = (props) => {
 						<DataTable.Row>
 							<DataTable.Cell>Cough</DataTable.Cell>
 							<DataTable.Cell>
-								{name[props.vals.ini_symptoms.cough]}
+								{name[props.vals.ini_symptoms?.cough]}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Chest Tightness</DataTable.Cell>
 							<DataTable.Cell>
-								{name[props.vals.ini_symptoms.chest_tightness]}{" "}
+								{name[props.vals.ini_symptoms?.chest_tightness]}{" "}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
@@ -250,21 +259,21 @@ const SymptonInfo = (props) => {
 								Night Time Awakening
 							</DataTable.Cell>
 							<DataTable.Cell>
-								{name[props.vals.ini_symptoms.nighttime]}
+								{name[props.vals.ini_symptoms?.nighttime]}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Restriction</DataTable.Cell>
 							<DataTable.Cell>
-								{name[props.vals.ini_symptoms.restriction]}
+								{name[props.vals.ini_symptoms?.restriction]}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Observations</DataTable.Cell>
 
-							{props.vals.ini_symptoms.observations ? (
+							{props.vals.ini_symptoms?.observations ? (
 								<DataTable.Cell>
-									{props.vals.ini_symptoms.observations}
+									{props.vals.ini_symptoms?.observations}
 								</DataTable.Cell>
 							) : (
 								<DataTable.Cell>No Observations</DataTable.Cell>
@@ -282,29 +291,29 @@ const SymptonInfo = (props) => {
 						<DataTable.Row>
 							<DataTable.Cell>Pulse</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.comorbidities.pulse}
+								{props.vals.comorbidities?.pulse}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Saturation</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.comorbidities.saturation}
+								{props.vals.comorbidities?.saturation}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Blood Pressure</DataTable.Cell>
 							<DataTable.Cell>
 								Sys:{" "}
-								{props.vals.comorbidities.blood_pressure_sys}
+								{props.vals.comorbidities?.blood_pressure_sys}
 								{"  "}
 								Dia:{" "}
-								{props.vals.comorbidities.blood_pressure_dia}
+								{props.vals.comorbidities?.blood_pressure_dia}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Respiratory Rate</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.comorbidities.resp_rate}
+								{props.vals.comorbidities?.resp_rate}
 							</DataTable.Cell>
 						</DataTable.Row>
 					</DataTable>
@@ -339,25 +348,25 @@ const SymptonInfo = (props) => {
 						<DataTable.Row>
 							<DataTable.Cell>HB</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.tests.cbc.hb}
+								{props.vals.tests?.cbc?.hb}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>WBC</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.tests.cbc.wbc}
+								{props.vals.tests?.cbc?.wbc}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>Eosinophils</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.tests.cbc.eosin}
+								{props.vals.tests?.cbc?.eosin}
 							</DataTable.Cell>
 						</DataTable.Row>
 						<DataTable.Row>
 							<DataTable.Cell>AEC</DataTable.Cell>
 							<DataTable.Cell>
-								{props.vals.tests.cbc.aec}
+								{props.vals.tests?.cbc?.aec}
 							</DataTable.Cell>
 						</DataTable.Row>
 					</DataTable>
@@ -376,13 +385,15 @@ const SymptonInfo = (props) => {
 							<DataTable.Row>
 								<DataTable.Cell>FEV1</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.pre
-										? props.vals.tests.spirometry.pre.fev1
+									{props.vals.tests?.spirometry?.pre
+										? props.vals.tests?.spirometry?.pre
+												?.fev1
 										: "No data recorded"}
 								</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.post
-										? props.vals.tests.spirometry.post.fev1
+									{props.vals.tests?.spirometry?.post
+										? props.vals.tests?.spirometry?.post
+												?.fev1
 										: "No data recorded"}
 								</DataTable.Cell>
 							</DataTable.Row>
@@ -390,20 +401,20 @@ const SymptonInfo = (props) => {
 								<DataTable.Cell>FEV1 Range</DataTable.Cell>
 								<DataTable.Cell>
 									<Text style={{ flex: 1, flexWrap: "wrap" }}>
-										{props.vals.tests.spirometry.pre
+										{props.vals.tests?.spirometry?.pre
 											? spiro[
-													props.vals.tests.spirometry
-														.pre.fev1_range
+													props.vals.tests?.spirometry
+														?.pre?.fev1_range
 											  ]
 											: "No data recorded"}
 									</Text>
 								</DataTable.Cell>
 								<DataTable.Cell>
 									<Text style={{ flex: 1, flexWrap: "wrap" }}>
-										{props.vals.tests.spirometry.post
+										{props.vals.tests?.spirometry?.post
 											? spiro[
-													props.vals.tests.spirometry
-														.post.fev1_range
+													props.vals.tests?.spirometry
+														?.post?.fev1_range
 											  ]
 											: "No data recorded"}
 									</Text>
@@ -412,31 +423,33 @@ const SymptonInfo = (props) => {
 							<DataTable.Row>
 								<DataTable.Cell>FEV1/FVC</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.pre
-										? props.vals.tests.spirometry.pre.ratio
+									{props.vals.tests?.spirometry?.pre
+										? props.vals.tests?.spirometry?.pre
+												?.ratio
 										: "No data recorded"}
 								</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.post
-										? props.vals.tests.spirometry.post.ratio
+									{props.vals.tests?.spirometry?.post
+										? props.vals.tests?.spirometry?.post
+												?.ratio
 										: "No data recorded"}
 								</DataTable.Cell>
 							</DataTable.Row>
 							<DataTable.Row>
 								<DataTable.Cell>FEV1/FVC Range</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.pre
+									{props.vals.tests?.spirometry?.pre
 										? spiro[
-												props.vals.tests.spirometry.pre
-													.ratio_range
+												props.vals.tests?.spirometry
+													?.pre?.ratio_range
 										  ]
 										: "No data recorded"}
 								</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.post
+									{props.vals.tests?.spirometry?.post
 										? spiro[
-												props.vals.tests.spirometry.post
-													.ratio_range
+												props.vals.tests?.spirometry
+													?.post?.ratio_range
 										  ]
 										: "No data recorded"}
 								</DataTable.Cell>
@@ -444,26 +457,29 @@ const SymptonInfo = (props) => {
 							<DataTable.Row>
 								<DataTable.Cell>FVC</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.pre
-										? props.vals.tests.spirometry.pre.fvc
+									{props.vals.tests?.spirometry?.pre
+										? props.vals.tests?.spirometry?.pre?.fvc
 										: "No data recorded"}
 								</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.post
-										? props.vals.tests.spirometry.post.fvc
+									{props.vals.tests?.spirometry?.post
+										? props.vals.tests?.spirometry?.post
+												?.fvc
 										: "No data recorded"}
 								</DataTable.Cell>
 							</DataTable.Row>
 							<DataTable.Row>
 								<DataTable.Cell>MMEF</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.pre
-										? props.vals.tests.spirometry.pre.mmef
+									{props.vals.tests?.spirometry?.pre
+										? props.vals.tests?.spirometry?.pre
+												?.mmef
 										: "No data recorded"}
 								</DataTable.Cell>
 								<DataTable.Cell>
-									{props.vals.tests.spirometry.post
-										? props.vals.tests.spirometry.post.mmef
+									{props.vals.tests?.spirometry?.post
+										? props.vals.tests?.spirometry?.post
+												?.mmef
 										: "No data recorded"}
 								</DataTable.Cell>
 							</DataTable.Row>
@@ -475,9 +491,9 @@ const SymptonInfo = (props) => {
 					)}
 					<RowView>
 						<CustomOverline text={"X-Ray: "} />
-						{props.vals.tests.xray ? (
+						{props.vals.tests?.xray ? (
 							<Text style={{ marginTop: 6 }}>
-								{props.vals.tests.xray}
+								{props.vals.tests?.xray}
 							</Text>
 						) : (
 							<Text style={{ marginTop: 6 }}>
@@ -487,9 +503,9 @@ const SymptonInfo = (props) => {
 					</RowView>
 					<RowView>
 						<CustomOverline text={"PEFR: "} />
-						{props.vals.tests.pefr ? (
+						{props.vals.tests?.pefr ? (
 							<Text style={{ marginTop: 6 }}>
-								{props.vals.tests.pefr}
+								{props.vals.tests?.pefr}
 							</Text>
 						) : (
 							<Text style={{ marginTop: 6 }}>
@@ -499,9 +515,9 @@ const SymptonInfo = (props) => {
 					</RowView>
 					<RowView>
 						<CustomOverline text={"IGE: "} />
-						{props.vals.tests.ige ? (
+						{props.vals.tests?.ige ? (
 							<Text style={{ marginTop: 6 }}>
-								{props.vals.tests.ige}
+								{props.vals.tests?.ige}
 							</Text>
 						) : (
 							<Text style={{ marginTop: 6 }}>
@@ -523,9 +539,9 @@ const SymptonInfo = (props) => {
 					</RowView>
 					<RowView>
 						<CustomOverline text={"Observations: "} />
-						{props.vals.tests.observations ? (
+						{props.vals.tests?.observations ? (
 							<Text style={{ marginTop: 6, flexShrink: 1 }}>
-								{props.vals.tests.observations}
+								{props.vals.tests?.observations}
 							</Text>
 						) : (
 							<Text style={{ marginTop: 6 }}>
@@ -539,7 +555,7 @@ const SymptonInfo = (props) => {
 						<>
 							<CustomSubTitle text="Follow Ups" />
 							<CustomCard>
-								{props.vals.follow_up.map((f, i) => {
+								{props.vals.follow_up?.map((f, i) => {
 									return (
 										<React.Fragment key={i}>
 											<Text
@@ -616,7 +632,7 @@ const SymptonInfo = (props) => {
 																Occurence
 															</DataTable.Title>
 														</DataTable.Header>
-														{f.symptom.map(
+														{f.symptom?.map(
 															(sym, index) => {
 																return (
 																	<DataTable.Row
